856e38e598a488ec7d18247fb5ab8ba8bbc82063
josephinebarbra <102880523+josephinebarbra@users.noreply.github.com>
2022-04-04 02:31:49 +0000

    Add files via upload

--- index.php
+++ index.php
@@ -1,26 +1,17 @@
-<html>
-<body>
-      
-      <?php
-         $to = "josephinebarbra1@gmail.com";
-         $subject = "This is subject";
-         
-         $message = "<b>This is HTML message.</b>";
-         $message .= "<h1>This is headline.</h1>";
-         
-         $header = "From:adamsbekky9@gmaiol.com\r\n";
-         $header .= "Cc:nicoleanderson863@gmail.com \r\n";
-         $header .= "MIME-Version: 1.0\r\n";
-         $header .= "Content-type: text/html\r\n";
-         
-         $retval = mail ($to,$subject,$message,$header);
-         
-         if( $retval == true ) {
-            echo "Message sent successfully...";
-         }else {
-            echo "Message could not be sent...";
-         }
-      ?>
-      
-   </body>
-</html>
+<?php
+// Recipient
+$to = 'adamsbekky9@gmail.com'
+
+// subject
+$subject = 'this is our test email';
+
+//message
+$message = '<h1>Hi there</h1><p>Thanks for testing!</p>';
+
+// Headers
+$headers = "From: The sender Name <sender@johnmorrisonline.com>\r\n";
+$headers .= "Reply-to: nicoleanderson863@gmail.com\r\n";
+$headers .= "Content-type: text/html\r\n";
+
+// Send email
+mail($to, $subject, $message, $headers);
